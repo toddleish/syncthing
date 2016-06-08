90460b16e69535e1f05f9a0b5f87ce3ae7ab1b85
Lab1 <Lab1@Lab1-PC>
2016-06-08 15:41:55 -0600

    Added this line to build.go so syncthing will not launch console... fmt.Fprintf(b, " -H=windowsgui")

--- build.go
+++ build.go
@@ -605,6 +605,7 @@ func ldflags() string {
 	fmt.Fprintf(b, " -X main.BuildStamp%c%d", sep, buildStamp())
 	fmt.Fprintf(b, " -X main.BuildUser%c%s", sep, buildUser())
 	fmt.Fprintf(b, " -X main.BuildHost%c%s", sep, buildHost())
+	fmt.Fprintf(b, " -H=windowsgui")	
 	return b.String()
 }
 
